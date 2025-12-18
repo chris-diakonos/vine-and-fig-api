0cea9e85a96bd3ad33e4314607cd45974bb062b8
Christopher Patton <chris@diakonos.io>
2025-12-17 21:11:53 -0600

    Add wall end

--- src/app/services/sheathing_builder.py
+++ src/app/services/sheathing_builder.py
@@ -183,7 +183,16 @@ class SheathingBuilder:
                         board_x_position = bays[bay - 1]
                         print(f"Board x position: {board_x_position}")
                         board_x_positions.append(board_x_position)
-                    elif bay > 1:
+                    elif bay == horizontal_quantity:
+                        previous_bay = board_x_positions[bay - 2] + (bay_width / 2)
+                        current_bay = wall_length
+                        board_length = current_bay - previous_bay
+                        print(f"Board length: {board_length}")
+                        board_lengths.append(board_length)
+                        board_x_position = bays[bay - 1]
+                        print(f"Board x position: {board_x_position}")
+                        board_x_positions.append(board_x_position)
+                    elif bay > 1 and bay < horizontal_quantity:
                         previous_bay = board_x_positions[bay - 2] + (bay_width / 2)
                         current_bay = bays[bay - 1] - (bay_width / 2)
                         board_length = current_bay - previous_bay
