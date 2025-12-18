e1f47493033f007313f04ee9101b001911aed5b0
Christopher Patton <chris@diakonos.io>
2025-12-17 20:56:55 -0600

    Array index

--- src/app/services/sheathing_builder.py
+++ src/app/services/sheathing_builder.py
@@ -172,13 +172,13 @@ class SheathingBuilder:
             else:
                 horizontal_quantity = (bay_count + 1)
 
-                for bay in range(1,bay_count + 2):
+                for bay in range(1,bay_count + 1):
 
                     if bay == 1:
                         board_length = bays[0] - (bay_width / 2)
                     elif bay > 1:
-                        previous_bay = bays[bay - 2] + (bay_width / 2)
-                        current_bay = bays[bay - 1] - (bay_width / 2)
+                        previous_bay = bays[bay - 1] + (bay_width / 2)
+                        current_bay = bays[bay] - (bay_width / 2)
                         board_length = current_bay - previous_bay
                         board_lengths.append(board_length)
                         board_x_positions.append(bays[bay - 1])
